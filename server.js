da6259a97a9559f66edf175f41a34e4b05a4b74f
Kojin Glick <kojinglick@gmail.com>
2020-12-19 14:07:39 -0800

    env values

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = 3000 ;
 
 const app = express();
 
@@ -12,11 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    }
+);
 
-mongoose.connect("mongodb+srv://moonstripe:Pointer11@cluster0.aymy7.mongodb.net/Fitness_Tracker?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
